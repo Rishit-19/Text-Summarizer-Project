d13f2342d74e4c233664cd2f2ee39d7c38d45d1b
Rishit Kotian <rishit.kotian19@gmail.com>
2024-03-15 21:22:31 +0530

    dataset download

---
+++ main.py
@@ -0,0 +1,13 @@
+from TextSummarizer.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"-----stage{STAGE_NAME} started-----")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"-----stage {STAGE_NAME} completed-----\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
